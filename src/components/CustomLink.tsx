7cb096ec309362b030712d6b89de1ce0b5437b94
John Tönnäng <john_tonnang@hotmail.com>
2024-07-09 15:31:10 +0200

    Add a CustomLink component

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { LinkType } from '~types/index';
+
+interface LinkProps {
+  link: LinkType;
+}
+
+const CustomLink: React.FC<LinkProps> = ({ link }) => {
+  let href = '';
+  let ariaLabel = '';
+
+  switch (link.linkType) {
+    case 'internal':
+      href = link.elementId
+        ? `#${link.elementId.current}`
+        : `/${link.pageReference?.slug?.current}`;
+      ariaLabel = `Navigate to ${link.title}`;
+      break;
+    case 'external':
+      href = link.url || '#';
+      ariaLabel = `Visit ${link.title} website`;
+      break;
+    case 'email':
+      href = `mailto:${link.email}`;
+      ariaLabel = `Send email to ${link.title}`;
+      break;
+    case 'phone':
+      href = `tel:${link.phone}`;
+      ariaLabel = `Call ${link.title}`;
+      break;
+    default:
+      return null;
+  }
+
+  return (
+    <Link
+      href={href}
+      aria-label={ariaLabel}
+      target={link.linkType === 'external' ? '_blank' : undefined}
+      rel={link.linkType === 'external' ? 'noopener noreferrer' : undefined}
+    >
+      {link.icon ? (
+        <Image src={link.icon.url} alt={link.title} width={32} height={32} />
+      ) : (
+        link.title
+      )}
+    </Link>
+  );
+};
+
+export default CustomLink;
