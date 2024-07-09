049b2164a9193560acd8357d6f0bb66c3981b4ef
John Tönnäng <john_tonnang@hotmail.com>
2024-07-09 19:39:55 +0200

    Adjust height for navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,7 +15,7 @@ const Menu: React.FC<MenuProps> = ({ title, links }) => {
 
   return (
     <>
-      <nav className="bg-background-orange fixed left-0 top-0 z-30 flex h-12 w-full max-w-[1440px] items-center justify-between px-4 py-4 lg:fixed lg:left-1/2 lg:h-24 lg:-translate-x-1/2 lg:bg-transparent lg:pl-16 lg:pr-[68px]">
+      <nav className="bg-background-orange fixed left-0 top-0 z-30 flex h-16 w-full max-w-[1440px] items-center justify-between px-4 py-4 lg:fixed lg:left-1/2 lg:h-24 lg:-translate-x-1/2 lg:bg-transparent lg:pl-16 lg:pr-[68px]">
         <Link href="/" className="flex items-center gap-10">
           <CircleIcon className="text-yellow" width={24} height={24} />
           <span className="lg:text-24 hidden font-bold lg:block">{title}</span>
