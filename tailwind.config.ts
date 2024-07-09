e070bcbc5db49fccfd48d7021dd923a0bdc2b067
John Tönnäng <john_tonnang@hotmail.com>
2024-07-09 15:27:29 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'background-orange': '#FE6232',
+        'background-white': '#FCF2E8',
+        dark: '#1E1E1E',
+        yellow: '#E6FE74',
+      },
+      fontFamily: {
+        geist: ['var(--font-geist)', 'sans-serif'],
+        reenie: ['var(--font-reenie)', 'cursive'],
+      },
+      fontSize: {
+        72: '4.5rem',
+        48: '3rem',
+        40: '2.5rem',
+        36: '2.25rem',
+        32: '2rem',
+        24: '1.5rem',
+        16: '1rem',
+        14: '0.875rem',
+        12: '0.75rem',
       },
     },
   },
