dc4c9be6f93b68f37350755fade5a970f88cb409
John Tönnäng <john_tonnang@hotmail.com>
2024-07-10 12:39:50 +0200

    Add arrow icon and animation, add scroll behaviour smooth

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -13,10 +13,23 @@ const Menu: React.FC<MenuProps> = ({ title, links }) => {
     setMenuOpen(!isMenuOpen);
   };
 
+  const handleHomeClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    history.replaceState(null, '', '/');
+  };
+
   return (
     <>
       <nav className="bg-background-orange fixed left-0 top-0 z-30 flex h-16 w-full max-w-[1440px] items-center justify-between px-4 py-4 lg:fixed lg:left-1/2 lg:h-24 lg:-translate-x-1/2 lg:bg-transparent lg:pl-16 lg:pr-[68px]">
-        <Link href="/" className="flex items-center gap-10">
+        <Link
+          href="/"
+          className="flex items-center gap-10"
+          onClick={handleHomeClick}
+        >
           <CircleIcon className="text-yellow" width={24} height={24} />
           <span className="lg:text-24 hidden font-bold lg:block">{title}</span>
         </Link>
