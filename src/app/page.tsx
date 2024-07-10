63c0474353e92f43cfd7acd2e66990c002a67c09
John Tönnäng <john_tonnang@hotmail.com>
2024-07-10 18:42:00 +0200

    Update homepage to accept new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import CaseWhiteBg from '~components/CaseWhiteBg';
 import CaseOrangeBg from '~components/CaseOrangeBg';
 import Resume from '~components/Resume';
 import TwoColumnTextImage from '~components/TwoColumnTextImage';
+import CaseHero from '~components/CaseHero';
+import CaseDescriptionLeft from '~components/CaseDescriptionLeft';
+import CaseDescriptionRight from '~components/CaseDescriptionRight';
+import Illustration from '~components/Illustration';
 
 import { ComponentProps, ComponentMap, PageData } from '~types/index';
 
@@ -14,19 +18,21 @@ const componentMap: ComponentMap = {
   caseOrangeBg: CaseOrangeBg as FC<ComponentProps>,
   resume: Resume as FC<ComponentProps>,
   twoColumnTextImage: TwoColumnTextImage as FC<ComponentProps>,
+  caseHero: CaseHero as FC<ComponentProps>,
+  caseDescriptionLeft: CaseDescriptionLeft as FC<ComponentProps>,
+  caseDescriptionRight: CaseDescriptionRight as FC<ComponentProps>,
+  illustration: Illustration as FC<ComponentProps>,
 };
 
 export default async function Home() {
   const pageData: PageData = await getStartPageData();
 
   return (
-    <>
-      <main>
-        {pageData.pageBuilder?.map((block, index) => {
-          const Component = componentMap[block._type];
-          return Component ? <Component key={index} {...block} /> : null;
-        })}
-      </main>
-    </>
+    <main>
+      {pageData.pageBuilder?.map((block, index) => {
+        const Component = componentMap[block._type];
+        return Component ? <Component key={index} {...block} /> : null;
+      })}
+    </main>
   );
 }
