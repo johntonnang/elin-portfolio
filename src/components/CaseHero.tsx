d66cfd607f7d2eb4d16bcf2e217525f7f75ab7c8
John Tönnäng <john_tonnang@hotmail.com>
2024-07-11 21:17:04 +0200

    Render image if it exists

--- src/components/CaseHero.tsx
+++ src/components/CaseHero.tsx
@@ -26,16 +26,18 @@ const CaseHero: React.FC<CaseProps> = ({
     <div className="bg-background-orange mb-[60px] flex h-full w-full">
       <div className="relative mx-auto flex h-full w-screen max-w-[1440px] flex-col-reverse items-center justify-between gap-12 px-4 py-20 xl:h-screen xl:flex-row xl:gap-0 xl:pb-10 xl:pl-16 xl:pr-[137px]">
         <div className="h-auto max-h-[450px] w-full xl:h-full xl:max-h-full xl:w-full xl:max-w-[588px]">
-          <Image
-            src={image.image}
-            alt={image.alt}
-            width="0"
-            height="0"
-            sizes="100vw"
-            quality={100}
-            priority
-            className="h-auto max-h-[450px] w-full object-contain xl:h-full xl:max-h-full xl:w-auto xl:max-w-[588px]"
-          />
+          {image && (
+            <Image
+              src={image.image}
+              alt={image.alt}
+              width="0"
+              height="0"
+              sizes="100vw"
+              quality={100}
+              priority
+              className="h-auto max-h-[450px] w-full object-contain xl:h-full xl:max-h-full xl:w-auto xl:max-w-[588px]"
+            />
+          )}
         </div>
         <div className="flex w-full max-w-[487px] flex-col gap-8 xl:gap-6">
           <h1 className="text-32 xl:text-44 font-bold">{header}</h1>
