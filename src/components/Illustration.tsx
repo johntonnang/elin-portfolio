5cb08a87cecdb0aedf038eea6e3a1ab0054b7543
John Tönnäng <john_tonnang@hotmail.com>
2024-07-11 21:43:03 +0200

    Small adjustment to illustration component

--- src/components/Illustration.tsx
+++ src/components/Illustration.tsx
@@ -15,8 +15,8 @@ const Illustration: FC<IllustrationProps> = ({ description, image }) => {
   return (
     <div className="bg-background-orange relative mb-[126px] flex h-full flex-col items-center justify-center xl:mb-[138px]">
       <div className="mx-auto flex h-full w-full max-w-[1440px] flex-col items-center justify-center gap-6 p-4 xl:p-16">
-        {image && (
-          <div className="h-full w-full">
+        <div className="h-full w-full">
+          {image && (
             <Image
               src={image.image}
               alt={image.alt}
@@ -26,8 +26,8 @@ const Illustration: FC<IllustrationProps> = ({ description, image }) => {
               quality={100}
               className="h-auto w-full object-cover"
             />
-          </div>
-        )}
+          )}
+        </div>
         {description && <p className="text-16 font-semibold">{description}</p>}
       </div>
       <span className="bg-grey absolute -bottom-[94px] left-5 h-[62px] w-1 translate-x-1/2 after:content-[''] xl:-bottom-[106px] xl:left-auto xl:right-1/2 xl:h-[74px]" />
